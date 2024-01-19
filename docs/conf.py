e07d60ad2124495926e29202d08baa2b74641dbe
AmyGrimwood <amy@grimwood.me.uk>
2024-01-19 11:35:00 +0000

    replace lumache.py with our .py and change all references to it

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PannelAppDB'
+author = [{name = "Frederick Mercer", email = "frederick.mercer@postgrad.manchester.ac.uk"}{name = "Abigail Haddon", email = "abigail.haddon@postgrad.manchester.ac.uk"}{name = "Amy Grimwood", email = "amy.grimwood@postgrad.manchester.ac.uk"}]
 
 release = '0.1'
 version = '0.1.0'
